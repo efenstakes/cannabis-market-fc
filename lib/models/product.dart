2abad29cdbee286255651fe66877a2d1b328da3a
Efen Stakes <efenstakes101@gmail.com>
2020-05-11 22:22:20 +0300

    add a product model

---
+++ lib/models/product.dart
@@ -0,0 +1,22 @@
+
+
+
+class Product {
+  String id;
+  String name;
+  String description;
+  String priceRange;
+  String vibe;
+  String type; // hybrid or natural
+  String thc;
+
+  Product({
+    this.id,
+    this.name,
+    this.description,
+    this.priceRange,
+    this.vibe,
+    this.type,
+    this.thc,
+  });
+}
